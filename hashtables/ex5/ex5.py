734998f6e04f4d01b4d54f11ed45c7e34383f9ec
Alex Kemper <alex.j.kemper@gmail.com>
2020-10-02 13:14:54 -0400

    finder complete

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,14 +1,30 @@
 # Your code here
 
 
-
 def finder(files, queries):
     """
     YOUR CODE HERE
     """
     # Your code here
 
-    return result
+    chopped = {}
+
+    for i in range(len(files)):
+        folders = files[i].split('/')
+        if folders[-1] in chopped:
+            chopped[folders[-1]] = [*chopped[folders[-1]], i]
+        if folders[-1] not in chopped:
+            chopped[folders[-1]] = [i]
+
+    results = []
+
+    for query in queries:
+        if query in chopped:
+            for i in range(len(chopped[query])):
+                print(chopped[query])
+                results.append(files[chopped[query][i]])
+
+    return results
 
 
 if __name__ == "__main__":
