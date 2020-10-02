3cb24d71cce685166e7de0a5e7354dab520e5dd9
Alex Kemper <alex.j.kemper@gmail.com>
2020-10-02 13:34:16 -0400

    get_indices finished

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -7,9 +7,23 @@ def get_indices_of_item_weights(weights, length, limit):
     store = {}
 
     for i in range(len(weights)):
+        if weights[i] in store:
+            store[weights[i]] = [*store[weights[i]], i]
         if weights[i] not in store:
-            store[weights[i]] = i
+            store[weights[i]] = [i]
+
+    print(store)
+    results = []
 
     for k, v in store.items():
+        if limit - k in store:
+            for i in range(len(store[limit-k])):
+                results.append(v[i])
 
-    return None
+    results.sort(reverse=True)
+
+    if results == []:
+        results = None
+
+    print(results)
+    return results
