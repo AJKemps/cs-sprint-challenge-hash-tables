d1a5106267698730963479496261728626b55b16
Alex Kemper <alex.j.kemper@gmail.com>
2020-10-02 11:59:40 -0400

    reconstruct trip is done

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -4,4 +4,12 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     # Your code here
 
+    store = {}
+
+    for i in range(len(weights)):
+        if weights[i] not in store:
+            store[weights[i]] = i
+
+    for k, v in store.items():
+
     return None
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -11,4 +11,29 @@ def reconstruct_trip(tickets, length):
     """
     # Your code here
 
-    return route
+    tix = {}
+
+    for ticket in tickets:
+        if ticket not in tix:
+            tix[ticket.source] = ticket.destination
+
+    routes = []
+
+    count = 0
+    finished = False
+
+    while not finished:
+        for k, v in tix.items():
+            print(k, v, count)
+            if count < 1:
+                if k == 'NONE':
+                    routes.append(v)
+                    count += 1
+            if count > 0:
+                if routes[count-1] == k:
+                    routes.append(v)
+                    count += 1
+                if routes[count-1] == 'NONE':
+                    finished = True
+
+    return routes
